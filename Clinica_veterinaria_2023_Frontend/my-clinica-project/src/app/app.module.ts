06881d7b4e69ae880694dea51eb3ec31d34b772b
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-02 19:11:13 -0300

    front feat: add page login

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/app.module.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
