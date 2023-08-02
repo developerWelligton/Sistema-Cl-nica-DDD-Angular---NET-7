31f3a8dc6abb124631f5c03c0c796fb1c073ffad
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-02 18:15:22 -0300

    feat: add cors

--- Clininca_veterinaria_2023_DDD/WebApi/Program.cs
+++ Clininca_veterinaria_2023_DDD/WebApi/Program.cs
@@ -101,6 +101,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//CORS
+var devClient = "http://localhost:4200";
+
+app.UseCors(x =>
+x.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader()
+.WithOrigins(devClient)); 
 app.UseHttpsRedirection();
 
 
