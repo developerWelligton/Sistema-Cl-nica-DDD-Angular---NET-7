b22a87edf70801237d464a1ac44149f64ef86a7a
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-09 22:28:48 -0300

    configuration: adjust environment

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5272' // Altere para o endereço correto da sua API .NET
+  apiUrl: 'http://104.215.126.210:5272'
+  //apiUrl: 'http://localhost:5272' // LOCAL
 };
