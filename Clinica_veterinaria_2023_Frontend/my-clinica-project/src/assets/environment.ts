68bdab399340cb7f40236b57ba8642e70bf38ddb
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-10 00:13:53 -0300

    configuration: adjust host

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://104.215.126.210:5272'
-  //apiUrl: 'http://localhost:5272' // LOCAL
+  //apiUrl: 'http://104.215.126.210:5272'
+  apiUrl: 'http://localhost:5272' // LOCAL
 };
