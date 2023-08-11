2093a1835ece5c8a835c1fe0d789456652d62a97
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-11 01:31:17 -0300

    Revert "configuration: adjust"
    This reverts commit 55e4d47062197546a879b5f58d359177bb64876f.

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.prod.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/assets/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://104.215.126.210:5272' // Altere 'http' para 'https'
+  production: false,
+  apiUrl: 'https://104.215.126.210:5272' // Altere 'http' para 'https'
   //apiUrl: 'http://localhost:4200' // LOCAL
 };
