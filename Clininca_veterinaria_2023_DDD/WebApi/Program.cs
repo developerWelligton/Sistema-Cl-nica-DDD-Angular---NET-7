06fecff7e6f2d85a2943dcdcd2a430a3ec4041d7
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-10 21:55:19 -0300

    configuration: https

--- Clininca_veterinaria_2023_DDD/WebApi/Program.cs
+++ Clininca_veterinaria_2023_DDD/WebApi/Program.cs
@@ -108,7 +108,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 //builder.WebHost.UseUrls("http://20.228.138.7:5272");
 
-builder.WebHost.UseUrls("http://*:5272");
+builder.WebHost.UseUrls("https://*:5272");
 
 var app = builder.Build();
  
