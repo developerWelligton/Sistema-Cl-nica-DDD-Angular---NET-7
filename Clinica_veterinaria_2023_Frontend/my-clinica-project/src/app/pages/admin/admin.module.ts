d1c2bca6bd580a0574f454147ccb8f43eef1b2e1
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-14 19:48:03 -0300

    feat: add formGroup user-create admin

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/admin/admin.module.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/admin/admin.module.ts
@@ -3,23 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 import { SidebarModule } from 'src/app/components/sidebar/sidebar.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
     AdminComponent,
     CreateUserComponent,
+
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     NavbarModule,
     SidebarModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgSelectModule,  // para usar no formulário option
 
   ],
   providers: []
--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/admin/create-user/create-user.component.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/admin/create-user/create-user.component.ts
@@ -2,26 +2,53 @@
 
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';   // Replace with the actual path to your service
-import { AnimalService } from 'src/app/services/animal.service';
-import { ConsultService } from 'src/app/services/consult.service';
-import { VeterinarioService } from 'src/app/services/veterinario.service';
-interface Veterinario {
-  iD_Veterinario: number;
-  nome: string;
+
+
+export enum UserGroup {
+  Cliente = 'Cliente',
+  Veterinario = 'Veterinário',
+  Secretaria = 'Secretaria'
 }
+
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent {
+  createUserForm: FormGroup;
+
+  listUserGroup: { id: string, name: string }[] = [];
 
   constructor(
     private fb: FormBuilder,
   ) {}
 
   ngOnInit() {
-
+    this.createUserForm = this.fb.group({
+      user_nome: [''],
+      user_cpf: [''],
+      user_email: ['']
+    });
+    this.populateUserGroups();
+  }
+  private populateUserGroups(): void {
+    this.listUserGroup = Object.values(UserGroup).map(group => ({
+      id: group,
+      name: group
+    }));
   }
 
+  submitForm(){
+    const formData = this.createUserForm.value;
+    if (this.createUserForm.valid) {
+      // process form values here
+      console.log(formData)
+  } else {
+      // handle form invalid case
+      console.log(formData)
+  }
+
+
+  }
 }
