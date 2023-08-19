5a668539a4fc12fb5e372691a2f0561e9a867f93
welligton-xplanning <welligton.costa@xplanning.com.br>
2023-08-19 20:47:14 -0300

    feat front: add alert toast

--- Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/login/login.component.ts
+++ Clinica_veterinaria_2023_Frontend/my-clinica-project/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { AuthService } from './../../services/auth.service';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
@@ -13,22 +13,28 @@ import { LoginService } from 'src/app/services/login.service';
 })
 
 export class LoginComponent {
-
+  isLoading = false;
   constructor(
     public formBuilder: FormBuilder,
     private router:Router,
     private loginService:LoginService,
-    public authService:AuthService
+    public authService:AuthService,
+    private cdr: ChangeDetectorRef
     ){
   }
 
   loginForm: FormGroup;
+  //
+  showAlert = false; // indica se o alerta deve ser exibido ou não
+alertMessage = ''; // a mensagem do alerta
+alertType = 'primary'; // 'primary' para sucesso, 'danger' para erro
 
   ngOnInit():void{
     this.loginForm = this.formBuilder.group({
       email:['',[Validators.required, Validators.email]],
       senha:['',[Validators.required]]
     })
+    this.cdr.detectChanges(); // força a detecção de mudança
   }
 
   get dadosForm(){
@@ -36,29 +42,48 @@ export class LoginComponent {
   }
 
   loginUser() {
-    this.isLoading = true;  // Mostrar spinner
-  
+    this.isLoading = true;
+
     this.loginService.login(this.dadosForm["email"].value, this.dadosForm["senha"].value).subscribe(
-      token => {
-        debugger
-        this.authService.setToken(token);
-        this.authService.UsuarioAutenticado(true);
-        this.router.navigate(['/dashboard']);
-        this.isLoading = false;  // Esconder spinner
-      },
-      err => {
-        debugger
-        //alert("Ocorreu um erro");
-        this.router.navigate(['/login']);
-        this.isLoading = false;  // Esconder spinner
-      }
+        token => {
+            this.authService.setToken(token);
+            this.authService.UsuarioAutenticado(true);
+            this.router.navigate(['/dashboard']);
+            this.isLoading = false;
+
+            // Mostrar alerta de sucesso
+            // this.showAlert = true;
+            // this.alertType = 'success';
+            // this.alertMessage = 'Login realizado com sucesso!';
+
+            // Esconder alerta após 3 segundos
+            setTimeout(() => {
+                this.showAlert = false;
+            }, 3000);
+        },
+        err => {
+            this.router.navigate(['/login']);
+            this.isLoading = false;
+
+            // Mostrar alerta de erro
+            this.showAlert = true;
+            this.alertType = 'danger';
+            this.alertMessage = 'Erro ao fazer login, verifique suas credenciais.';
+
+            // Esconder alerta após 3 segundos
+            setTimeout(() => {
+                this.showAlert = false;
+            }, 3000);
+        }
     );
   }
+
+
   
 
   color: ThemePalette = 'primary';
   mode: ProgressSpinnerMode = 'determinate';
   value = 50;
 
-  isLoading = false; 
+  
 }
